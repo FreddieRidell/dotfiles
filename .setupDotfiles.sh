911dd30d66220acf25ebeaa14583ddf3775cd5f1
Freddie Ridell <freddie@codogo.io>
2017-02-20 10:08:33 +0000

    added setup script

---
+++ .setupDotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+git clone --bare git@github.com:CodogoFreddie/dotfiles.git $HOME/dotfiles
+config config --local status.showUntrackedFiles no<Paste>
+config status
