ab8fce6b00ffde4ac14c4d1ae33c6084d5d0fd0b
Freddie Ridell <freddie@codogo.io>
2017-03-14 18:22:08 +0000

    fix(prompt): there's something wrong with the origin diff printer

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -4,23 +4,23 @@ promptinit
 
 #Left Prompt
 function get_current_hostname(){
-	echo "%F{red}%m"
+	echo "%{%F{red}%}%m"
 }
 
 function get_current_dir(){
-	echo "%F{green}%~"
+	echo "%{%F{green}%}%~"
 }
 
 function get_current_repo(){
 	if git rev-parse --git-dir > /dev/null 2>&1; then
-		echo "%F{white}(%F{magenta}$(basename `git rev-parse --show-toplevel`)%F{white})"
+		echo "%{%F{white}%}(%{%F{magenta}%}$(basename `git rev-parse --show-toplevel`)%{%f%})"
 	else
 		echo ""
 	fi
 }
 
 function get_left_prompt(){
-	echo "$(get_current_hostname) $(get_current_repo) $(get_current_dir) \n%f$ "
+	echo "$(get_current_hostname) $(get_current_repo) $(get_current_dir) %f$ "
 }
 
 
@@ -29,14 +29,15 @@ function get_git_branch(){
 	echo "%F{magenta}$(git rev-parse --abbrev-ref HEAD)"
 }
 
+REMOVE_WHITESPACE='s/[ \t]*//g'
 function get_git_count(){
-	CHECKED="$(git status --porcelain 2>/dev/null| grep "^$1" | wc -l)"
-	UNCHECKED="$(git status --porcelain 2>/dev/null| grep "^$2" | wc -l)"
+	CHECKED="$(git status --porcelain 2>/dev/null| grep "^$1" | wc -l | sed -e $REMOVE_WHITESPACE )"
+	UNCHECKED="$(git status --porcelain 2>/dev/null| grep "^$2" | wc -l | sed -e $REMOVE_WHITESPACE )"
 
 	if [ $CHECKED = "0" ] && [ $UNCHECKED = "0" ] ; then
 		echo ""
 	else
-		echo "%F{$3}$UNCHECKED%f/%F{$3}$CHECKED %f| " 
+		echo "%{%F{$3}%}$UNCHECKED%{%f%}/%{%F{$3}%}$CHECKED %{%f%}| " 
 	fi
 }
 
@@ -44,10 +45,8 @@ function get_git_diff_origin_num(){
 	BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 	if [ $BRANCH != "HEAD" ]; then
-		REGEX_AHEAD="s/\\s\\+\\S\\+//"
-		REGEX_BEHIND="s/\\S\\+\\s\\+//"
-		NUM_AHEAD="$(git rev-list --left-right --count $BRANCH...origin/$BRANCH 2> /dev/null | sed -e $REGEX_AHEAD)"
-		NUM_BEHIND="$(git rev-list --left-right --count $BRANCH...origin/$BRANCH 2> /dev/null | sed -e $REGEX_BEHIND)"
+		NUM_AHEAD="$(git rev-list --left-right --count $BRANCH...origin/$BRANCH 2> /dev/null | sed -e $REMOVE_WHITESPACE )"
+		NUM_BEHIND="$(git rev-list --left-right --count $BRANCH...origin/$BRANCH 2> /dev/null | sed -e $REMOVE_WHITESPACE )"
 
 		if [ $NUM_AHEAD != "0" ] || [ $NUM_BEHIND != "0" ] ; then
 			echo "%F{cyan}$NUM_AHEAD%f/%F{cyan}$NUM_BEHIND %f| " 
@@ -61,7 +60,7 @@ function get_git_diff_origin_num(){
 
 function get_right_prompt(){
 	if git rev-parse --git-dir > /dev/null 2>&1; then
-		echo "%f[ $(get_git_count 'A' '??' 'green')$(get_git_count 'M' ' M' 'yellow')$(get_git_count 'D' ' D' 'red')$(get_git_diff_origin_num)$(get_git_branch)%f ]"
+		echo "%f[ $(get_git_count 'A' '??' 'green')$(get_git_count 'M' ' M' 'yellow')$(get_git_count 'D' ' D' 'red')$(get_git_branch)%f ]"
 	else
 		echo "[]"
 	fi
