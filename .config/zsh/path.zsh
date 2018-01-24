dd7dbe810bb98cf7a3b37ba7cbe37cc671cd4bcb
Freddie Ridell <freddie@codogo.io>
2018-01-24 09:14:09 +0000

    feat(rustup): installed rustup, added to path, moved path definitions to their own file

---
+++ .config/zsh/path.zsh
@@ -0,0 +1,15 @@
+#good defaults
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH"
+
+#android stuff
+export PATH="$PATH:$ANDROID_HOME/tools"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+
+#Various Yarn global install paths
+export PATH="$HOME/.yarn/bin:$PATH"
+export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+export PATH="./node_modules/.bin:$PATH" 
+
+#rustup
+export PATH="$HOME/.cargo/bin:$PATH"
+
