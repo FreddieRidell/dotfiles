ae7d8dd6cc1375540243274106dcc13ada1ddd72
Freddie Ridell <freddie.ridell@gmail.com>
2018-04-15 15:22:37 +0100

    Update .setupDotfiles.sh

--- .setupDotfiles.sh
+++ .setupDotfiles.sh
@@ -4,6 +4,7 @@ alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:CodogoFreddie/dotfiles.git $HOME/dotfiles
 config config --local status.showUntrackedFiles no
 git reset --hard master
+git reset --hard HEAD
 config status
 
 chsh -s $( which zsh ) $USER
