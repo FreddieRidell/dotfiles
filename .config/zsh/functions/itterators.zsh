3bc0c63af00ab6114d6e3ae7a675db8b68790c19
Freddie Ridell <freddie@codogo.io>
2018-07-04 11:51:14 +0100

    broke zsh functions out into seperate folders

---
+++ .config/zsh/functions/itterators.zsh
@@ -0,0 +1,37 @@
+function vimovergrep {
+	echo "vimovergrep is a work in progress"
+	echo "$1"
+	echo "$2"
+}
+
+function freddieFixFormat {
+	for f in $( yarn run format | grep \"/home/freddie\" ) ; do vim $f ; done
+}
+
+function openXinY {
+	ag -l "$1" "$2"
+	ag -l "$1" "$2" | xargs -n 1 vim
+}
+
+
+function findReplaceIn {
+  ag -l $1 $3 | xargs -n 1 -I '{}'   sed --in-place '{}' -e "s/$1/$2/g" 
+}
+
+function vimFileAndTest {
+  file="$1"
+  testDir="$(dirname "$1")/__tests__"
+  testFile="$(basename "$1")"
+  testPath="$testDir/$( echo $testFile| sed -e "s/\.js/\.spec\.js/" )"
+  vim -O $file $testPath
+}
+
+function vimFilesAndTests {
+  for x in $@ ; do
+    file=$x
+    testDir="$(dirname "$x")/__tests__"
+    testFile="$(basename "$x")"
+    testPath="$testDir/$( echo $testFile| sed -e "s/\.js/\.spec\.js/" )"
+    vim -O $file $testPath
+  done
+}
