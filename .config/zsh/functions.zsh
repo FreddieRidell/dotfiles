b52517de48da68ac9b2ee09f23d37966711636f0
Freddie Ridell <freddie@codogo.io>
2018-07-04 12:23:22 +0100

    fixed path issue in functions.zsh

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,3 +1,3 @@
-source ./functions/itterators.zsh
-source ./functions/shortcuts.zsh
-source ./functions/pass.zsh
+source $HOME/.config/zsh/functions/itterators.zsh
+source $HOME/.config/zsh/functions/shortcuts.zsh
+source $HOME/.config/zsh/functions/pass.zsh
