6c1f293a52df40b61a4ab9bd292761bb81271188
Freddie Ridell <freddie@codogo.io>
2018-07-18 12:07:39 +0100

    tweaked lockscript

--- .i3/lock.sh
+++ .i3/lock.sh
@@ -2,6 +2,6 @@
 
 IMG_NAME="/tmp/$RANDOM.png" 
 scrot $IMG_NAME
-convert -resize 12.5% -scale 800% $IMG_NAME $IMG_NAME
+convert -scale 12.5% -scale 800% $IMG_NAME $IMG_NAME
 i3lock -i $IMG_NAME
 rm $IMG_NAME
