2db4cbf6de15ca220e0fccd25453ceb958c085f7
Freddie Ridell <freddie@codogo.io>
2018-10-05 11:30:53 +0100

    tried to add hypertask shortcut

---
+++ .i3/hypertaskRofiScript.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ x"$@" = x"quit" ]
+then
+    exit 0
+fi
+
+if [ "$@" ]
+then
+	RESP=`task add $*`
+
+
+	while read -r line; do
+		echo -e "$line"
+	done <<< "$RESP"
+else
+	RESP=`task`
+
+	while read -r line; do
+		echo -e "$line"
+	done <<< "$RESP"
+fi
