c70a53df3afade2688eb5294ac60dd3a112e0b54
Freddie Ridell <contact@freddieridell.com>
2018-11-29 15:11:21 +0000

    feat(zsh): aliased cd -> pushd

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -16,3 +16,5 @@ alias control-center="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
 alias settings-daemon="env XDG_CURRENT_DESKTOP=GNOME gnome-settings-daemon"
 
 alias ls="ls --color=auto --group-directories-first -p"
+alias cd="pushd"
+alias back="popd"
