37c33e7a9f17e9731f24fe254a58136a74815db8
Freddie Ridell <contact@freddieridell.com>
2018-12-14 10:47:30 +0000

    added some new zsh aliases

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -15,4 +15,6 @@ alias node="nodejs"
 alias control-center="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
 alias settings-daemon="env XDG_CURRENT_DESKTOP=GNOME gnome-settings-daemon"
 
-alias ls="ls --color=auto --group-directories-first -p"
+alias ls="ls -G"
+alias less="less -R"
+alias exa="exa -la"
