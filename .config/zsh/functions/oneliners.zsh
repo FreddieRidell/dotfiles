15b053300cc6e5c1c921d4e42eff451f81486e9c
Freddie Ridell <contact@freddieridell.com>
2019-01-22 09:40:06 +0000

    don't use colored ag output in scripts

--- .config/zsh/functions/oneliners.zsh
+++ .config/zsh/functions/oneliners.zsh
@@ -1,21 +1,21 @@
 function cabalCabal { cabal --key cabal://7d99b453506b9743bf5e71fe749f66c814d7cd9388a5d394a27eed4c5640302b --nick freddieRidell } 
 function cabalDUCSGS { cabal --key dat://88a978f3ce3bd7c7e9aecfc4bf19d34b2ae44b0e2356c295a995163cd3aa2e9e --nick freddieRidell } 
 function chpwd { title "$( getFolder )" } 
-function findAndReplaceInFolder { ag -l $1 | xargs sd -i $1 $2 }
-function findPretty() { for FILE_NAME in $( ag -l $1 ) ; do ; scriptMsg $FILE_NAME ; bat --color always --decorations always $FILE_NAME | ag --color $1 -C ${2:-2} ; done | less -R }
+function findAndReplaceInFolder { ag --nocolor -l $1 | xargs sd -i $1 $2 }
+function findPretty() { for FILE_NAME in $( ag --nocolor -l $1 ) ; do ; scriptMsg $FILE_NAME ; bat --color always --decorations always $FILE_NAME | ag --color $1 -C ${2:-2} ; done | less -R }
 function getFolder () { echo ${\PWD##*/} } 
 function gitCurrentBranch { git symbolic-ref -q --short HEAD }
 function gitPoke { git commit --amend --date="now" --no-edit > /dev/null ; git rev-parse HEAD } 
 function gitResetToOrigin { git reset --hard "origin/$( gitCurrentBranch )" } 
 function gitRid { git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done } 
-function gitStatusSorted { git status | sort | ag .\+ --no-color } 
+function gitStatusSorted { git status | sort | ag --nocolor .\+ --no-color } 
 function isoDate { date +"%Y-%m-%d" } 
 function isoTime { date +"%Y-%m-%d %H:%M:%S" }
 function jqModify { TMP_FILE_NAME="/tmp/$RANDOM.json" && jq $1 $2 > $TMP_FILE_NAME && mv $TMP_FILE_NAME $2 } 
 function largestFile { wc -l "$@" | sort -nr -t":" -k1 | head -2 | tail -1 | sed -e 's/[0-9]\+//' }
 function lesss { cats $1 | less -r } 
 function lock { ~/.i3/lock.sh } 
-function openXinY { ag -l "$1" "$2" ; ag -l "$1" "$2" | xargs -n 1 nvim -c "silent! /$1" } 
+function openXinY { ag -l "$1" "$2" ; ag -l --nocolor "$1" "$2" | xargs -n 1 nvim -c "silent! /$1" } 
 function printAndEval() { scriptMsg $@ ; $@ } 
 function randomHex { echo $RANDOM | xargs printf "%x\n" }
 function reactLifecycleCheatsheet { bat ~/.config/cheatsheets/react.md }
