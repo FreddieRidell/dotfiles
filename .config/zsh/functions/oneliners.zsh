c3870df28f66bce2413108f14e43004628539bd1
Freddie Ridell <contact@freddieridell.com>
2019-03-20 08:45:15 +0000

    fix(zsh/gitBranchify): remove trailing dashes

--- .config/zsh/functions/oneliners.zsh
+++ .config/zsh/functions/oneliners.zsh
@@ -4,7 +4,7 @@ function chpwd { title "$( getFolder )" }
 function findAndReplaceInFolder { ag --nocolor -l $1 | xargs sd -i $1 $2 }
 function findPretty() { for FILE_NAME in $( ag --nocolor -l $1 ) ; do ; scriptMsg $FILE_NAME ; bat --color always --decorations always $FILE_NAME | ag --color $1 -C ${2:-2} ; done | less -R }
 function getFolder () { echo ${\PWD##*/} } 
-function gitBranchify { git checkout -b "$1-$( echo "${@:2}" | sed -e "s/[^a-zA-Z0-9]/-/g" -e "s/--*/-/g" | tr '[:upper:]' '[:lower:]' )" }
+function gitBranchify { git checkout -b "$1-$( echo "${@:2}" | sed -e "s/[^a-zA-Z0-9]/-/g" -e "s/--*/-/g" -e "s/-$//g" | tr '[:upper:]' '[:lower:]' )" }
 function gitCheckoutSearch { git branch | ag "$@" | strip-ansi | xargs -n 1 git checkout }
 function gitCurrentBranch { git symbolic-ref -q --short HEAD }
 function gitCurrentHash { git rev-parse HEAD }
