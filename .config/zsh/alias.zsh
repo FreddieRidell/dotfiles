157176017e6698efd5f6bef4954533371fb10330
Freddie Ridell <contact@freddieridell.com>
2019-04-14 15:15:29 +0100

    added feh to alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,22 +1,17 @@
-alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
-alias cats='highlight -O ansi'
-
-alias vim="nvim"
-
-alias '..'='cd ..'
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-
-alias treee="tree --dirsfirst -C"
-
-alias node="nodejs"
-
-alias control-center="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
-alias settings-daemon="env XDG_CURRENT_DESKTOP=GNOME gnome-settings-daemon"
-
-alias ls="ls --color"
-alias less="less -R"
-alias exa="exa -la --git --extended --header --time-style=long-iso"
 alias ag="ag --color"
+alias cats='highlight -O ansi'
+alias '..'='cd ..'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias control-center="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
+alias exa="exa -la --git --extended --header --time-style=long-iso"
+alias feh="feh -FZ"
 alias forEach="xargs -n 1"
+alias -g .....='../../../..'
+alias -g ....='../../..'
+alias -g ...='../..'
+alias less="less -R"
+alias ls="ls --color"
+alias node="nodejs"
+alias settings-daemon="env XDG_CURRENT_DESKTOP=GNOME gnome-settings-daemon"
+alias treee="tree --dirsfirst -C"
+alias vim="nvim"
