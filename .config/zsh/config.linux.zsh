3b58fdf5954674c59af92c9c4f56fe3a847ededd
Freddie Ridell <contact@freddieridell.com>
2019-10-21 16:16:29 +0100

    broke zsh config out into os specific files

---
+++ .config/zsh/config.linux.zsh
@@ -0,0 +1,4 @@
+alias ls="ls --color"
+
+#Force US keyboard layout
+setxkbmap us
--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -26,9 +26,6 @@ compinit
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 setopt COMPLETE_ALIASES
 
-#Force US keyboard layout
-setxkbmap us
-
 #screen layouts
 if [ "$HOST" = "ln1-pl-2086" ]; then
 	xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
