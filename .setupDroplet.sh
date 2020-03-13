1b491ac143dc8502294b942f02f44ecfbedb3b17
Freddie Ridell <contact@freddieridell.com>
2020-03-13 10:36:21 +0000

    Update .setupDroplet.sh

--- .setupDroplet.sh
+++ .setupDroplet.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+BOLD=$(tput bold)
+CLEAR=$(tput sgr0)
+
+echo Creating user $BOLDfreddie
 adduser freddie
+
+echo Adding $BOLDfreddie$CLEAR to group $BOLDsudo
 usermod -aG sudo freddie
-su - freddie
-ssh-keygen
+
+echo Generating ssh key for $BOLDfreddie
+su freddie -c ssh-keygen
+
+echo Setting up config for $BOLDfreddie
+su freddie -c 'sh -c "$(wget https://raw.githubusercontent.com/CodogoFreddie/dotfiles/master/.setupDotfiles.sh -O -)"'
+su - freddie 
