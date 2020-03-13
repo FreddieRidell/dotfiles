d7ade2ed403c5438911ed293ae4397927bdf71f6
Freddie Ridell <contact@freddieridell.com>
2020-03-13 10:43:09 +0000

    Update .setupDroplet.sh

--- .setupDroplet.sh
+++ .setupDroplet.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+echo
 echo Creating user freddie
 adduser freddie
 
+echo
 echo Adding freddie to group sudo
 usermod -aG sudo freddie
 
+echo
 echo Generating ssh key for freddie
 su freddie -c ssh-keygen
 
-echo Setting up config for freddie
-su freddie -c 'sh -c "$(wget https://raw.githubusercontent.com/CodogoFreddie/dotfiles/master/.setupDotfiles.sh -O -)"'
+echo
+echo now paste the following ssh key into github
+cat /home/freddie/.ssh/id_rsa.pub
+echo and the run the following command to setup config:
+echo 'sh -c "$(wget https://raw.githubusercontent.com/CodogoFreddie/dotfiles/master/.setupDotfiles.sh -O -)"'
+
 su - freddie 
