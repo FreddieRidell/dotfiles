13b34678451d1092c1b2395757971aff29ab4907
Freddie Ridell <contact@freddieridell.com>
2020-09-15 15:42:47 +0100

    sorted alias file

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,18 +1,18 @@
+alias ag="ag --color"
+alias cats='highlight -O ansi'
 alias '..'='cd ..'
+alias control-center="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
+alias exa="exa -la --git --extended --header --time-style=long-iso"
+alias feh="feh -FZ"
+alias forEach="xargs -n 1"
 alias -g .....='../../../..'
 alias -g ....='../../..'
 alias -g ...='../..'
 alias -g config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 alias -g cortex='/usr/bin/git --git-dir=$HOME/cortex/ --work-tree=$HOME'
-alias ag="ag --color"
-alias cats='highlight -O ansi'
-alias control-center="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
-alias exa="exa -la --git --extended --header --time-style=long-iso"
-alias feh="feh -FZ"
-alias forEach="xargs -n 1"
 alias less="less -R"
 #alias node="nodejs"
 alias settings-daemon="env XDG_CURRENT_DESKTOP=GNOME gnome-settings-daemon"
+alias tmux1080p="tmux select-layout '7aac,319x88,0,0{235x88,0,0,0,83x88,236,0[83x28,236,0,4,83x28,236,29,5,83x30,236,58,6]}'"
 alias treee="tree --dirsfirst -C"
 alias vim="nvim"
-alias tmux1080p="tmux select-layout '7aac,319x88,0,0{235x88,0,0,0,83x88,236,0[83x28,236,0,4,83x28,236,29,5,83x30,236,58,6]}'"
