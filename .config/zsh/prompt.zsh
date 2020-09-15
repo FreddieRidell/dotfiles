e37aca336068719b1b940ca589d16a59b65f5811
Freddie Ridell <contact@freddieridell.com>
2020-09-15 19:23:32 +0100

    setup an +ACTIVE task shell readout

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -4,8 +4,16 @@ autoload -Uz vcs_info
 
 promptinit
 
-PROMPT="%F{$PROMPT_COLOR}%n@%m%f %F{7}%D{%a %Y-%m-%d %T}%f %F{2}%~%f %F{7}
-$ %f"
+function current_started_task {
+	TASKS="$(task +ACTIVE export)"
+	LENGTH="$(echo $TASKS | jq 'length')"
+	if [ $LENGTH -ne "0" ] ; then
+		echo $TASKS | jq -r  '.[0] | "[%F{2}\(.id)%f] %F{5}\(.project)%f %F{3}\( .tags | map( "+\(.)") | join(" ") )%f %F{6}\( .description )%f"'
+	fi
+}
+
+PROMPT='%F{$PROMPT_COLOR}%n@%m%f %F{7}%D{%a %Y-%m-%d %T}%f %F{2}%~%f %F{7}$(current_started_task)
+$ %f'
 
 RPROMPT='$(rusty-zsh-prompt --right)'
 
