53502b1be08289085ef7128881ff6d67a7841a4c
Freddie Ridell <contact@freddieridell.com>
2020-12-27 15:55:56 +0000

    tempoarily removed taskwarrior prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -5,27 +5,27 @@ autoload -Uz vcs_info
 promptinit
 
 function current_started_task {
-	TASKS="$(task +ACTIVE export)"
-	LENGTH="$(echo $TASKS | jq 'length')"
-	if [ $LENGTH -ne "0" ] ; then
-		echo $TASKS | jq -r  '
-		.[0] | [
-			"[%F{2}\(.id)%f]",
+	#TASKS="$(task +ACTIVE export)"
+	#LENGTH="$(echo $TASKS | jq 'length')"
+	#if [ $LENGTH -ne "0" ] ; then
+		#echo $TASKS | jq -r  '
+		#.[0] | [
+			#"[%F{2}\(.id)%f]",
 
-			if select(.project) then
-				"%F{5}\( .project? )%f"
-			else null end,
+			#if select(.project) then
+				#"%F{5}\( .project? )%f"
+			#else null end,
 
-			if select(.tags) then
-				"%F{3}\( .tags | map( "+\(.)") | join(" ") )%f"
-			else null end,
+			#if select(.tags) then
+				#"%F{3}\( .tags | map( "+\(.)") | join(" ") )%f"
+			#else null end,
 
-			"%F{4}\( .description )%f"
+			#"%F{4}\( .description )%f"
 
-		] |  map(select(.)) | map( "\(.)" ) | join(" ")'
-	else 
-		echo "%K{1}%F{8} NO ACTIVE TASK %f%k"
-	fi
+		#] |  map(select(.)) | map( "\(.)" ) | join(" ")'
+	#else 
+		#echo "%K{1}%F{8} NO ACTIVE TASK %f%k"
+	#fi
 }
 
 function pwd_color {
