bf759d6b03036f03c02299dbc3f2990c58bec573
Freddie Ridell <contact@freddieridell.com>
2021-05-16 08:58:01 +0000

    task wip

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -4,37 +4,13 @@ autoload -Uz vcs_info
 
 promptinit
 
-function current_started_task {
-	#TASKS="$(task +ACTIVE export)"
-	#LENGTH="$(echo $TASKS | jq 'length')"
-	#if [ $LENGTH -ne "0" ] ; then
-		#echo $TASKS | jq -r  '
-		#.[0] | [
-			#"[%F{2}\(.id)%f]",
-
-			#if select(.project) then
-				#"%F{5}\( .project? )%f"
-			#else null end,
-
-			#if select(.tags) then
-				#"%F{3}\( .tags | map( "+\(.)") | join(" ") )%f"
-			#else null end,
-
-			#"%F{4}\( .description )%f"
-
-		#] |  map(select(.)) | map( "\(.)" ) | join(" ")'
-	#else 
-		#echo "%K{1}%F{8} NO ACTIVE TASK %f%k"
-	#fi
-}
-
 function pwd_color {
 	HASH="0x$(echo $PWD | md5)"
 	HEX=${HASH[1,8]}
 	print $(( 1 + HEX % 6))
 }
 
-PROMPT='%F{$PROMPT_COLOR}%n@%m%f %F{7}%D{%a %Y-%m-%d %T}%f %F{$(pwd_color)}%~%f %F{7}$(current_started_task)
+PROMPT='%F{$PROMPT_COLOR}%n@%m%f %F{7}%D{%a %Y-%m-%d %T}%f %F{$(pwd_color)}%~%f
 $ %f'
 
 RPROMPT='$(rusty-zsh-prompt --right)'
